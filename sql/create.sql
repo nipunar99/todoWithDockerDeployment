c565e006446f271e96f2cf5132a43377a1be2865
Nipuna Rahal <2020cs019@stu.ucsc.cmb.ac.lk>
2023-11-20 01:22:02 +0530

    sql change

--- sql/create.sql
+++ sql/create.sql
@@ -1,8 +1,5 @@
-CREATE TABLE IF NOT EXISTS 'todo'
-    ('id' int(11) NOT NULL AUTO_INCREMENT,
-                           'description' text NOT NULL,
-                                              'created_at' datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                                                                                     'updated_at' datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-                                                                                                                                                        PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT
-CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
+CREATE TABLE IF NOT EXISTS todo
+    ( id SERIAL PRIMARY KEY,
+                        description TEXT NOT NULL,
+                                         created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                                                               updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP);
