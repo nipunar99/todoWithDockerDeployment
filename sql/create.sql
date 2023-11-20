b00cbeab408f0284f162d5d91e3939564862fa0e
Nipuna Rahal <2020cs019@stu.ucsc.cmb.ac.lk>
2023-11-20 12:08:11 +0530

    sql script change

--- sql/create.sql
+++ sql/create.sql
@@ -1,5 +1,19 @@
-CREATE TABLE IF NOT EXISTS todo
-    ( id SERIAL PRIMARY KEY,
-                        description TEXT NOT NULL,
-                                         created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                                                                               updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP);
+CREATE TABLE IF NOT EXISTS todo ( id SERIAL PRIMARY KEY,
+                                                    description TEXT NOT NULL,
+                                                                     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                                                                                           updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP);
+
+-- Add a trigger to update updated_at on every update
+
+CREATE OR REPLACE FUNCTION update_updated_at() RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = CURRENT_TIMESTAMP;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER update_todo_updated_at
+BEFORE
+UPDATE ON todo
+FOR EACH ROW EXECUTE FUNCTION update_updated_at();
